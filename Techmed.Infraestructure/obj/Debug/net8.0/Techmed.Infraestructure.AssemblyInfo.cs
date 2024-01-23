1eaf23a3ff7d11d3ed59c8cd4e564ca4f93d6670
kayquepiton <kayquepiton@gmail.com>
2024-01-23 17:17:22 -0300

    dependencias

--- Techmed.Infraestructure/obj/Debug/net8.0/Techmed.Infraestructure.AssemblyInfo.cs
+++ Techmed.Infraestructure/obj/Debug/net8.0/Techmed.Infraestructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Techmed.Infraestructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8d67934fe99c214fa9046318485fc52783ca937")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+158537791c6fdb3c89122129d1e5f7b3012afa86")]
 [assembly: System.Reflection.AssemblyProductAttribute("Techmed.Infraestructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Techmed.Infraestructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
