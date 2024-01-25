ea93f0057b11b9baff507fa846c78d6d76bc9631
kayquepiton <kayquepiton@gmail.com>
2024-01-25 14:43:25 -0300

    application

---
+++ Techmed.Application/InputModels/NewPacienteInputModel.cs
@@ -0,0 +1,10 @@
+namespace TechMed.Application.InputModels
+{
+    public class NewPacienteInputModel
+    {
+        public string Nome { get; set; }
+        public string Endereco { get; set; }
+        public string Telefone { get; set; }
+        // Inclua outras propriedades conforme necessário
+    }
+}
