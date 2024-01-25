89ef3295e8d0268d601e1363725a6ff48a1f68bb
kayquepiton <kayquepiton@gmail.com>
2024-01-25 17:25:28 -0300

    Swagger

--- TechMed.Application/ViewModels/AtendimentoViewModel.cs
+++ TechMed.Application/ViewModels/AtendimentoViewModel.cs
@@ -5,8 +5,8 @@ namespace TechMed.Application.ViewModels
         public int AtendimentoId { get; set; }
         public DateTime DataHora { get; set; }
         public int MedicoId { get; set; }
-        public string? NomeMedico { get; set; }
+        public string? NomeMedico { get; set; } // Alteração feita aqui
         public int PacienteId { get; set; }
-        public string? NomePaciente { get; set; }
+        public string? NomePaciente { get; set; } // Alteração feita aqui
     }
 }
