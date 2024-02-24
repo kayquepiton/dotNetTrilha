ae94ada99da663560e421d2f87669cc7b7c1088a
kayquepiton <kayquepiton@gmail.com>
2024-02-24 12:16:16 -0300

    add Relacionamentos

---
+++ TechMed_EFCore/Migrations/20240224151154_CriacaoTabelaMedicos.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TechMedEFCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriacaoTabelaMedicos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Atendimentos_MedicoId",
+                table: "Atendimentos",
+                column: "MedicoId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Atendimentos_Medicos_MedicoId",
+                table: "Atendimentos",
+                column: "MedicoId",
+                principalTable: "Medicos",
+                principalColumn: "MedicoId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Atendimentos_Medicos_MedicoId",
+                table: "Atendimentos");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Atendimentos_MedicoId",
+                table: "Atendimentos");
+        }
+    }
+}
---
+++ TechMed_EFCore/Model.cs
@@ -0,0 +1,80 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace TechMed_EFCore.Models;
+public class TechMedContext : DbContext
+{
+
+    public DbSet<Medico> Medicos { get; set; }
+    public DbSet<Paciente> Pacientes { get; set; }
+    public DbSet<Atendimento> Atendimentos { get; set; }
+    public DbSet<Exame> Exames { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        var connectionString = "server=localhost;user=root;password=tic2023;database=techmed";
+        var serverVersion = ServerVersion.AutoDetect(connectionString);
+        
+        optionsBuilder.UseMySql(connectionString, serverVersion);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Medico>().ToTable("Medicos")
+            .HasKey(m => m.MedicoId);
+        modelBuilder.Entity<Paciente>().ToTable("Pacientes")
+            .HasKey(p => p.PacienteId);
+        modelBuilder.Entity<Atendimento>().ToTable("Atendimentos")
+            .HasKey(a => a.AtendimentoId);
+
+        modelBuilder.Entity<Paciente>()
+            .HasMany(p => p.Atendimentos)
+            .WithOne(a => a.Paciente)
+            .HasForeignKey(a => a.PacienteId);
+
+    }
+    // codigo omitido
+}
+
+public abstract class Pessoa{
+    public required string Nome {get; set;}
+    public required string CPF {get; set;}
+}
+
+public class Medico : Pessoa{
+    public int MedicoId {get; set;}
+    public required string CRM {get; set;}
+    public string? Especialidade {get; set;}
+    public required decimal Salario {get; set;}
+}
+
+public class Paciente : Pessoa{
+    public int PacienteId {get; set;}
+    public string? Endereco {get; set;}
+    public string? Telefone {get; set;}
+    public ICollection<Atendimento> Atendimentos {get;} = new List<Atendimento>();
+}
+
+public class Atendimento{
+    public int AtendimentoId {get; set;}
+    public required DateTime DataHora {get; set;}
+    public required string Suspeita {get; set;}
+    public string? Diagnostico {get; set;}
+    
+    public required int MedicoId {get; set;}
+    public required Medico Medico {get; set;}
+    public required int PacienteId {get; set;}
+    public required Paciente Paciente {get; set;}
+
+    public ICollection<Exame>? Exames {get; set;}
+}
+
+public class Exame{
+    public int ExameId {get; set;}
+    public required string Titulo {get; set;}
+    public decimal Valor {get; set;}
+    public string? Descricao {get; set;}
+    public required int AtendimentoId {get; set;}
+    public required Atendimento Atendimento {get; set;}
+}
