7bdd6de48fb8242bd083ebf3279e81635e670093
kayquepiton <kayquepiton@gmail.com>
2024-02-26 17:17:37 -0300

    Mvc_SimpleAuth

--- MvcMovie/obj/Debug/net7.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net7.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7197ab46d4876e29e6416fcfa84a423026aebf9c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec6269d1399d14f4dfcdfc48bc7bb8bc34ec794c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
